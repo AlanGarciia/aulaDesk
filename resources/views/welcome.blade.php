895ef27f78b48cd24fd54286be6c5504ff08af07
Alan Garcia <alan.garcia@insbaixcamp.cat>
2026-01-30 16:30:18 +0100

    Pruebaaa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,6 +87,7 @@
                                 <span class="flex items-center justify-center rounded-full bg-[#FDFDFC] dark:bg-[#161615] shadow-[0px_0px_1px_0px_rgba(0,0,0,0.03),0px_1px_2px_0px_rgba(0,0,0,0.06)] w-3.5 h-3.5 border dark:border-[#3E3E3A] border-[#e3e3e0]">
                                     <span class="rounded-full bg-[#dbdbd7] dark:bg-[#3E3E3A] w-1.5 h-1.5"></span>
                                 </span>
+                                
                             </span>
                             <span>
                                 Watch video tutorials at
